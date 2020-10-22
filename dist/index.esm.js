a8d09f419c7aa3d625a76db1f776cb58285caf91
Boy Hagemann <boyhagemann@MacBook-Pro-van-Boy.local>
2020-10-22 22:08:11 +0200

    Testing with dist folder

---
+++ dist/index.esm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var Editor = function () {
+    return (React.createElement("div", null, "Editor!!!"));
+};
+
+export { Editor };
+//# sourceMappingURL=index.esm.js.map
---
+++ dist/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var Editor = function () {
+    return (React__default['default'].createElement("div", null, "Editor!!!"));
+};
+
+exports.Editor = Editor;
+//# sourceMappingURL=index.js.map
